7d5320009d4090faf1531056b137ed29fffdeb5d
MauroArg <mauro66arg@gmail.com>
2021-03-23 16:46:36 -0600

    First commit

---
+++ SessionApi/SessionApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SessionApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ SessionApi/SessionApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SessionApi.WebApiApplication" Language="C#" %>
